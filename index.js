67f652e99d0b75419a697eecf3a43bfb8506b938
unknown <anajuliaalvesmota@gmail.com>
2025-11-13 20:52:14 -0300

    feat: setup inicial da API Node.js e configuracao de CI/CD

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['']; 
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.json({ "mensagem": "API online e integrada com CI/CD!" });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
