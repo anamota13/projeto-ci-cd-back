8208f07be70f292853aa1a0a134a173db5b5267a
unknown <anajuliaalvesmota@gmail.com>
2025-11-13 22:06:13 -0300

    fix: ajustando o index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const allowedOrigins = ['https://projeto-ci-cd-front-ten.vercel.app']; 
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); 
+        } else {
+            callback(new Error(`Not allowed by CORS: ${origin}`));
+        }
     }
-  }
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); 
 
 app.get('/', (req, res) => {
-  res.json({ "mensagem": "API online e integrada com CI/CD!" });
+    res.json({ "mensagem": "API online e integrada com CI/CD - Configuração Restrita de CORS (v1.0.5)!" });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
